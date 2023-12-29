239f3547e0891c6e7a4423d07d3bcf8aea2d497d
cbg7144 <128574611+cbg7144@users.noreply.github.com>
2023-12-29 12:34:51 +0900

    tap2구현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ViewPager2 viewPager;
+    private PagerAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        setViewPager();
+        setTabLayout();
+    }
+
+    private void setViewPager(){
+        viewPager = binding.pager;
+        pagerAdapter = new PagerAdapter(this);
+        pagerAdapter.createFragment(0);
+        pagerAdapter.createFragment(1);
+        pagerAdapter.createFragment(2);
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    private void setTabLayout(){
+
+        tabLayout = binding.tabLayout;
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText("탭" + (position + 1))
+        ).attach();
 
     }
 }
