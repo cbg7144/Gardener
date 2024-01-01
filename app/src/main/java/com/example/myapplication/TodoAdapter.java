7abbba2f3de90dc9a7ea74c92f52c4b91bc4f74c
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-01 22:58:55 +0900

    Feat : Tab3 구현

--- app/src/main/java/com/example/myapplication/TodoAdapter.java
+++ app/src/main/java/com/example/myapplication/TodoAdapter.java
@@ -1,11 +1,13 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,17 +21,19 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
         protected TextView todoName;
         protected Button deleteButton;
+        protected CheckBox checkBox;
 
         public ViewHolder(View itemView) {
             super(itemView);
             todoName = itemView.findViewById(R.id.todoItem);
             deleteButton = itemView.findViewById(R.id.deleteBtn);
+            checkBox = itemView.findViewById(R.id.check);
 
             deleteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int position = getAdapterPosition(); // 현재 어댑터가 다루고 있는 리스트의 포지션을 가져옴
-                    if(position != RecyclerView.NO_POSITION){ // 삭제된 포지션이 아닌 경우
+                    if (position != RecyclerView.NO_POSITION) { // 삭제된 포지션이 아닌 경우
                         mData.remove(position); // 해당 포지션의 item을 제거
                         notifyItemRemoved(position); // 어댑터에게 데이터셋이 변경되었음을 알림
                     }
@@ -56,11 +60,26 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
         return viewHolder;
     }
 
-    // position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.todoName.setText(mData.get(position).getTodoName()); // 직접적으로 binding
+        TodoItem currentItem = mData.get(position);
+        holder.todoName.setText(currentItem.getTodoName());
+
+        holder.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            currentItem.setIsChecked(isChecked);
+
+            // 취소선 추가/제거
+            if (isChecked) {
+                holder.todoName.setPaintFlags(holder.todoName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                holder.todoName.setTextColor(0xFFAAAAAA);
+
+            } else {
+                holder.todoName.setPaintFlags(holder.todoName.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                holder.todoName.setTextColor(0xFF000000);
+            }
+        });
     }
+
 
     // 전체 데이터 갯수 리턴
     @Override
